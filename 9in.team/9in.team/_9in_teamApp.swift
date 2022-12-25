dc80ae6e0855588bf5e63ef5dbabd8af33361bc4
cho1490 <chosanghyun99@gmail.com>
2022-12-25 19:01:32 +0900

    chore: 프로젝트 생성

---
+++ 9in.team/9in.team/_9in_teamApp.swift
@@ -0,0 +1,17 @@
+//
+//  _in_teamApp.swift
+//  9in.team
+//
+//  Created by 조상현 on 2022/12/25.
+//
+
+import SwiftUI
+
+@main
+struct _9in_teamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 9in.team/9in.teamUITests/_in_teamUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  _in_teamUITestsLaunchTests.swift
+//  9in.teamUITests
+//
+//  Created by 조상현 on 2022/12/25.
+//
+
+import XCTest
+
+final class _in_teamUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
