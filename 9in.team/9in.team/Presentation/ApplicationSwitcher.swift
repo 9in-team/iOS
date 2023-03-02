9858c135ccb57ae5e59fc8800827479eebc90456
cho1490 <chosanghyun99@gmail.com>
2023-03-02 22:01:25 +0900

    feat: 앱 시작 화면 수정

---
+++ 9in.team/9in.team/Presentation/ApplicationSwitcher.swift
@@ -0,0 +1,25 @@
+//
+//  File.swift
+//  9in.team
+//
+//  Created by 조상현 on 2023/02/28.
+//
+
+import SwiftUI
+
+struct ApplicationSwitcher: View {
+    
+    @StateObject var viewModel = SignViewModel()
+    
+    var body: some View {
+        if viewModel.isSingIn {
+                MainView()
+        } else {
+            SignIn()
+                .onAppear {
+                    viewModel.autoLogin()
+                }
+        }
+        
+    }
+}
