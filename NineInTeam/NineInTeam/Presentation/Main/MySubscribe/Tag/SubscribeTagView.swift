889aa341d1e5aee93bd75f95601af3b294eb93c4
HeonJin Ha <weizen511@gmail.com>
2023-04-26 21:58:39 +0900

    feat: 구독하기 화면 NaviagtionBar 구성

--- NineInTeam/NineInTeam/Presentation/Main/MySubscribe/Tag/SubscribeTagView.swift
+++ NineInTeam/NineInTeam/Presentation/Main/MySubscribe/Tag/SubscribeTagView.swift
@@ -8,32 +8,38 @@
 import SwiftUI
 
 struct SubscribeTagView: View {
-
+    
     let navigationTitle = "구독하기"
-
+    
+    @State var currentTab: Int = 0
+    
     @ObservedObject var viewModel = SubscribeTagViewModel()
+    
 }
 
 extension SubscribeTagView {
-
+    
     var body: some View {
         BaseView(appState: viewModel.appState) {
             mainBody()
         }
-        .showNavigationBar(NavigationBar(useDismissButton: true, title: navigationTitle))
+        .showTabNavigationBar(NavigationBar(useDismissButton: true, title: navigationTitle),
+                              TabNavigationBar(tabList: ["스터디", "프로젝트"]) { selectedIndex in
+            currentTab = selectedIndex
+        })
     }
-
+    
     func mainBody() -> some View {
         VStack(spacing: 16) {
             SubscribeTagCell(isSubscribe: .constant(false))
-
+            
             SubscribeTagCell(isSubscribe: .constant(true))
-
+            
             Spacer()
         }
         .padding(.horizontal, 20)
     }
-
+    
 }
 
 struct SubscribeTagView_Previews: PreviewProvider {
