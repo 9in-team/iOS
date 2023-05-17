862ccbc0010967835997f0a60484db9252d6c285
HeonJin Ha <weizen511@gmail.com>
2023-05-17 16:49:55 +0900

    feat: Team 불러오기 XCTest 초기화

---
+++ NineInTeam/NineInTeamTests/NineInTeamTestFetchTeams.swift
@@ -0,0 +1,25 @@
+//
+//  NineInTeamTestFetchTeams.swift
+//  NineInTeamTests
+//
+//  Created by Heonjin Ha on 2023/05/17.
+//
+
+import XCTest
+import Combine
+@testable import NineInTeam
+
+final class NineInTeamTestFetchTeams: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+    }
+
+    func testFetchTeams() {
+
+    }
+}
