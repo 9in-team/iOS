e7d36d95e1c8244e9308a386ae6eab99a37cebf0
HeonJin Ha <weizen511@gmail.com>
2023-05-19 00:07:49 +0900

    chore: 테스트 도메인 추가

--- NineInTeam/NineInTeam/Repository/UrlType.swift
+++ NineInTeam/NineInTeam/Repository/UrlType.swift
@@ -9,6 +9,7 @@ enum UrlType {
     
     case test
     case testLocal
+    case testDomain
 
     func get() -> String {
         switch self {
@@ -16,6 +17,8 @@ enum UrlType {
             return "http://223.130.134.106:8080/"
         case .testLocal:
             return "http://127.0.0.1:8080/"
+        case .testDomain:
+            return "https://9inteam.heon.dev/"
         }
     }
     
