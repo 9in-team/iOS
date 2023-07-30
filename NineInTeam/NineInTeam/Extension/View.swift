6aee16e5fdfa545d9600a03f675badbd0eeea800
HeonJin Ha <weizen511@gmail.com>
2023-07-30 18:22:52 +0900

    fix: 그림자 표시되지 않는 문제 수정

--- NineInTeam/NineInTeam/Extension/View.swift
+++ NineInTeam/NineInTeam/Extension/View.swift
@@ -69,7 +69,7 @@ extension View {
                 ZStack {
                     ForEach(shadows, id: \.self) { shadow in
                         RoundedRectangle(cornerRadius: cornerRadius)
-                            .fill(.white.opacity(0.01))
+                            .fill(.white)
                             .shadow(color: shadow.getColor(), radius: shadow.radius,
                                     x: shadow.locationX, y: shadow.locationY)
                     }
